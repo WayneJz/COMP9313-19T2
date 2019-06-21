6bc13552a15b6ccb54f6c79082dcccfa9b147e2e
WayneJz <waynezjiang@gmail.com>
2019-06-21 20:51:43 +1000

    ass1 uploaded

---
+++ assignment1/src/main/java/assignment1/Assignment1.java
@@ -0,0 +1,12 @@
+package assignment1;
+
+/**
+ * 
+ * This class solves the problem posed for Assignment1
+ *
+ */
+public class Assignment1 {
+  
+// TODO: Write the source code for your solution here.	
+	
+}
